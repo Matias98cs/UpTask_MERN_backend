aebef6c4e7c3a20cfe494c19487ac2a371a6199a
Matias98cs <matias98cs@gmail.com>
2023-01-18 11:54:49 -0300

    creando model de usuario

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv'
 const app = express()
 dotenv.config()
 conectarDB()
-app.listen(4000, () => {
-    console.log(`Servidor corriendo en el puerto 4000`)
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
 })
