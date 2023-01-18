bb7c678f4a318abbe1f79f09ae2c6b3baaf54d96
Matias98cs <matias98cs@gmail.com>
2023-01-18 12:25:38 -0300

    creadon carpeta de rutas y rutas para usuarios

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import conectarDB from './config/db.js';
 import dotenv from 'dotenv'
+import usuariosRoutes from './routes/usuariosRoutes.js'
 
 const app = express()
 dotenv.config()
 conectarDB()
+
+//Routing
+app.use('/api/usuarios', usuariosRoutes)
+
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`)
