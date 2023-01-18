23ac8b0d1754e916491735d7defafd23d1bd1511
Matias98cs <matias98cs@gmail.com>
2023-01-18 13:13:35 -0300

    crea un usuario y lo guarda en la DB y evita registrar duplicados

---
+++ controllers/usuarioController.js
@@ -0,0 +1,19 @@
+import Usuario from "../models/Usuario.js";
+
+const registrar = async (req, res) => {
+  const { email } = req.body;
+  const existeUsuario = await Usuario.findOne({ email })
+  if(existeUsuario){
+    const error = new Error('Usuario ya registrado')
+    return res.status(400).json({msg: error.message})
+  }
+  try {
+    const usuario = new Usuario(req.body);
+    const usuarioAlmacenado = await usuario.save();
+    return res.json({ msg: "Usuario almacenado" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { registrar };
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import usuariosRoutes from './routes/usuariosRoutes.js'
 
 const app = express()
+app.use(express.json())
 dotenv.config()
 conectarDB()
 
