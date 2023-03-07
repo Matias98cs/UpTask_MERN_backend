5eca3d4f13f9edf897ee3164dd57303223ecb6ed
Matias98cs <matias98cs@gmail.com>
2023-03-07 15:55:30 -0300

    metodos para el controlador de proyectos

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,45 +1,101 @@
 import Proyecto from "../models/Proyecto.js";
 
 const obtenerProyectos = async (req, res) => {
-
-}
+  //solo busca los proyectos que fueron creados por el mismo usuario
+  const proyectos = await Proyecto.find().where("creador").equals(req.usuario);
+  res.status(200).json(proyectos);
+};
 
 const nuevoProyecto = async (req, res) => {
-
-}
+  const proyecto = new Proyecto(req.body);
+  proyecto.creador = req.usuario._id;
+  try {
+    const proyectoAlmacenado = await proyecto.save();
+    res.status(200).json(proyectoAlmacenado);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const obtenerProyecto = async (req, res) => {
+  const { id } = req.params;
+  const proyecto = await Proyecto.findById(id);
+  if (!proyecto) {
+    const error = new Error("El proyecto no existe o no se encontro");
+    return res.status(404).json({ msg: error.message });
+  }
 
-}
+  //compara los id del creador para poder acceder a la informacion
+  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Accion no valida");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  res.json(proyecto);
+};
 
 const editarProyecto = async (req, res) => {
+  const { id } = req.params;
+  const proyecto = await Proyecto.findById(id);
+  if (!proyecto) {
+    const error = new Error("El proyecto no existe o no se encontro");
+    return res.status(404).json({ msg: error.message });
+  }
 
-}
+  //compara los id del creador para poder acceder a la informacion
+  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Accion no valida");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  proyecto.nombre = req.body.nombre || proyecto.nombre;
+  proyecto.descripcion = req.body.descripcion || proyecto.descripcion;
+  proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega;
+  proyecto.cliente = req.body.cliente || proyecto.cliente;
+
+  try {
+    const proyectoAlmacenado = await proyecto.save();
+    res.status(200).json(proyectoAlmacenado);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const eliminarProyecto = async (req, res) => {
+  const { id } = req.params;
+  const proyecto = await Proyecto.findById(id);
+  if (!proyecto) {
+    const error = new Error("El proyecto no existe o no se encontro");
+    return res.status(404).json({ msg: error.message });
+  }
 
-}
+  //compara los id del creador para poder acceder a la informacion
+  if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Accion no valida");
+    return res.status(404).json({ msg: error.message });
+  }
 
-const agregarColaborador = async (req, res) => {
+  try {
+    await proyecto.deleteOne();
+    res.status(200).json({ msg: "Proyecto eliminado" });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-}
+const agregarColaborador = async (req, res) => {};
 
-const eliminarColaborador = async (req, res) => {
+const eliminarColaborador = async (req, res) => {};
 
-}
-
-const obtenerTareas = async (req, res) => {
-
-}
-
+const obtenerTareas = async (req, res) => {};
 
 export {
-    obtenerProyectos,
-    nuevoProyecto,
-    obtenerProyecto,
-    editarProyecto,
-    eliminarProyecto,
-    agregarColaborador,
-    eliminarColaborador,
-    obtenerTareas
-}
+  obtenerProyectos,
+  nuevoProyecto,
+  obtenerProyecto,
+  editarProyecto,
+  eliminarProyecto,
+  agregarColaborador,
+  eliminarColaborador,
+  obtenerTareas,
+};
